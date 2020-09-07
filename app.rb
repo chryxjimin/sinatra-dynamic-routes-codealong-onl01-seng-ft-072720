aebea4edc199e7447cc8fc640ab15313e0d2993c
Christie Yoon <cleex1999@gmail.com>
2020-09-07 21:37:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -23,6 +23,6 @@ class App < Sinatra::Base
      #binding.pry
       @number1 = params[:num1].to_i
       @number2 = params[:num2].to_i
-      "#{@number1} * #{@number2}".to_s
+      "#{@number1} * #{@number2}"
    end
 end
